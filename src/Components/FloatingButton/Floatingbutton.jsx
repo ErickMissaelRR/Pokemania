7407511609b7f412845796f90d634a582b0c7735
Erick Ruiz <emissael5@gmail.com>
2021-09-01 19:11:31 -0500

    fix bug floating-button

--- src/Components/FloatingButton/Floatingbutton.jsx
+++ src/Components/FloatingButton/Floatingbutton.jsx
@@ -2,38 +2,40 @@ import { FloatingMenu, MainButton, ChildButton } from 'react-floating-button-men
 import React, { useState } from 'react'
 import {MdAdd, MdClose} from 'react-icons/md';
 
+
 const Floatingbutton = () => {
 
-    const [open, setOpen] = useState(false)
+    const [isOpen, setOpen] = useState(false);
 
     return (
         <div>
             <FloatingMenu
                 slideSpeed={400}
-                size="1rem"
                 direction="up"
-                spacing={6}
+                spacing={8}
+                isOpen={isOpen}
+                position="fixed"
             >
                 <MainButton
                     iconResting={<MdAdd style={{ fontSize: 25 }} />}
                     iconActive={<MdClose style={{ fontSize: 25 }} />}
                     iconColor="black"
                     background="red"
-                    onClick={() => setOpen(!open)}
+                    onClick={() => setOpen(!isOpen)}
                     size="3rem"
                 />
                 <ChildButton
-                    iconButton={<MdAdd style={{ fontSize: 25 }} />}
+                    icon={<MdAdd style={{ fontSize: 25 }}
                     iconColor="black"
-                    backgroundColor="red"
-                    size={50}
+                    background="white"
+                    size="2rem"
                     onClick={() => console.log('Pokemania')}
                 />
                 <ChildButton
-                    iconButton={<MdAdd style={{ fontSize: 25 }} />}
+                    icon={<MdAdd style={{ fontSize: 25 }} />}
                     iconColor="black"
-                    backgroundColor="red"
-                    size={50}
+                    background="red"
+                    size="2rem"
                     onClick={() => console.log('PokeApi')}
                 />
             </FloatingMenu>
